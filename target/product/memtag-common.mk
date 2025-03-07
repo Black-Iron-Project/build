9aaa3c3d5370a1a20c09a01d6b6425c244ec1e8f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-07 08:24:47 +0800

    build: disable memory tagging for surfaceflinger
    Change-Id: Iebfb63569fd627f0da32d68f21863ab852af3081
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- target/product/memtag-common.mk
+++ target/product/memtag-common.mk
@@ -28,6 +28,9 @@ PRODUCT_MEMTAG_HEAP_ASYNC_DEFAULT_INCLUDE_PATHS := \
     system/bpf \
     system/netd/netutil_wrappers \
     system/netd/server
+    
+PRODUCT_MEMTAG_HEAP_EXCLUDE_PATHS := \
+    frameworks/native/services/surfaceflinger
 
 PRODUCT_MEMTAG_HEAP_EXCLUDE_PATHS := \
     frameworks/native/services/surfaceflinger
